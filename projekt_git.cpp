495137379aac332b21f9f84bb58ea483fe87eca9
Wirusik <150136013+Wirusik@users.noreply.github.com>
2023-11-21 12:28:05 +0100

    Dodano 3 Couty 2 ciny
    Proste program proszący użytkownika o podanie dwóch liczb po czym wypisanie wyniku sumy

--- projekt_git.cpp
+++ projekt_git.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 int a, b, c;
 int main() {
-
+	cout << "Podaj pierwsza liczbe: ";
+	cin >> a;
+	cout << "Podaj druga liczbe: ";
+	cin >> b;
+	c = a + b;
+	cout << "Wynik: " << c;
 	return 0;
 }
